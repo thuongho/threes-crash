9ea5716b554af46a44f8572f26be491013861e07
Thuong Ho <thuong.t.ho@gmail.com>
2016-05-02 00:33:54 -0700

    earth is there just no color

--- js/main.js
+++ js/main.js
@@ -36,7 +36,7 @@
     //   }
     // );
 
-    // instantiate a loader
+    // // instantiate a loader
     var loader = new THREE.JSONLoader();
 
     // load a resource
@@ -44,19 +44,55 @@
       // resource URL
       '../models/earth.json',
       // Function when resource is loaded
-      function ( geometry, materials ) {
+      function ( geometry, colors) {
         console.log(geometry);
-        console.log(materials);
+        console.log(colors);
+        var material = new THREE.MeshLambertMaterial( {color: colors} );
       //   var material = new THREE.MultiMaterial( materials );
-        // var object = new THREE.Mesh( geometry, material );
-        // scene.add( object );
+        var object = new THREE.Mesh( geometry, material );
+        scene.add( object );
       }
       // function(object) {
       //   scene.add(object);
       // }
     );
 
-    // camera.position.z = 3;
+    // instantiate a loader
+    // var loader = new THREE.ColladaLoader();
+
+    // loader.load(
+    //   // resource URL
+    //   '../models/earth.dae',
+    //   // Function when resource is loaded
+    //   function ( collada ) {
+    //     console.log(collada);
+    //     scene.add( collada.scene );
+    //   },
+    //   // Function called when download progresses
+    //   function ( xhr ) {
+    //     console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+    //   }
+    // );
+
+    // // // create shape
+    // var geometry = new THREE.BoxGeometry( 2, 2, 2 );
+
+    // // array of 6 diff material for the 6 faces of the cube
+    // var cubeMaterials = [
+    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/1.png'), side: THREE.DoubleSide}),  // RIGHT SIDE
+    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/2.png'), side: THREE.BackSide}),  // LEFT SIDE
+    //   new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide}),  // TOP SIDE
+    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/4.png'), side: THREE.DoubleSide}),  // BOTTOM SIDE
+    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/5.png'), side: THREE.FrontSide}),  // FRONT SIDE
+    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/6.png'), side: THREE.DoubleSide})  // BACK SIDE
+    // ];
+
+    // // create material, color or image texture
+    // var material = new THREE.MeshFaceMaterial(cubeMaterials);
+    // var cube = new THREE.Mesh( geometry, material );
+    // scene.add( cube );
+
+    camera.position.z = 10;
 
     // light (color, intensity)
     // MeshBasic doesn't apply to lighting
