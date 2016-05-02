6ad16f9993f9c37d9d6d92cfe58712cc284e5cbd
Thuong Ho <thuong.t.ho@gmail.com>
2016-05-02 01:28:12 -0700

    wip

--- js/main.js
+++ js/main.js
@@ -25,81 +25,27 @@
     // controls
     var controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-    // var loader = new THREE.OBJLoader();
-    // console.log('load');
-    // // Planet Earth (Alt-Drag to change Lighting) by Thomas Kole is licensed under CC Attribution
-    // // https://skfb.ly/Ft8J
-    // loader.load(
-    //   '../models/earth.json',
+    camera.position.z = 3;
 
-    //   function(object) {
-    //     console.log(object);
-    //     scene.add(object);
-    //   }
-    // );
+    // make the number big so that it doesn't get blurry
+    var geometry = new THREE.CubeGeometry(10000, 10000, 10000);
+    var cubeMaterials = [
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../img/flatrock/flatrock_ft.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../img/flatrock/flatrock_bk.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../img/flatrock/flatrock_up.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../img/flatrock/flatrock_dn.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../img/flatrock/flatrock_rt.png'), side: THREE.DoubleSide}),
+      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('../img/flatrock/flatrock_lf.png'), side: THREE.DoubleSide})
+    ];
 
-    // // instantiate a loader
-    var loader = new THREE.JSONLoader();
-
-    // load a resource
-    loader.load(
-      // resource URL
-      '../models/earth.json',
-      // Function when resource is loaded
-      function ( geometry, colors) {
-        console.log(geometry);
-        console.log(colors);
-        var material = new THREE.MeshLambertMaterial( {color: colors} );
-      //   var material = new THREE.MultiMaterial( materials );
-        var object = new THREE.Mesh( geometry, material );
-        scene.add( object );
-      }
-      // function(object) {
-      //   scene.add(object);
-      // }
-    );
-
-    // instantiate a loader
-    // var loader = new THREE.ColladaLoader();
-
-    // loader.load(
-    //   // resource URL
-    //   '../models/earth.dae',
-    //   // Function when resource is loaded
-    //   function ( collada ) {
-    //     console.log(collada);
-    //     scene.add( collada.scene );
-    //   },
-    //   // Function called when download progresses
-    //   function ( xhr ) {
-    //     console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
-    //   }
-    // );
-
-    // // // create shape
-    // var geometry = new THREE.BoxGeometry( 2, 2, 2 );
-
-    // // array of 6 diff material for the 6 faces of the cube
-    // var cubeMaterials = [
-    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/1.png'), side: THREE.DoubleSide}),  // RIGHT SIDE
-    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/2.png'), side: THREE.BackSide}),  // LEFT SIDE
-    //   new THREE.MeshBasicMaterial({color: 0xFFFFFF, side: THREE.DoubleSide}),  // TOP SIDE
-    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/4.png'), side: THREE.DoubleSide}),  // BOTTOM SIDE
-    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/5.png'), side: THREE.FrontSide}),  // FRONT SIDE
-    //   new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('img/6.png'), side: THREE.DoubleSide})  // BACK SIDE
-    // ];
-
-    // // create material, color or image texture
-    // var material = new THREE.MeshFaceMaterial(cubeMaterials);
-    // var cube = new THREE.Mesh( geometry, material );
-    // scene.add( cube );
-
-    camera.position.z = 10;
+    var cubeMaterial = new THREE.MeshFaceMaterial(cubeMaterials);
+    var cube = new THREE.Mesh(geometry, cubeMaterial);
+    scene.add(cube);
 
     // light (color, intensity)
     // MeshBasic doesn't apply to lighting
     // Lambert or Fong
-    var ambientLight = new THREE.AmbientLight(0xFFFFFF, 1.8);
+    var ambientLight = new THREE.AmbientLight(0xFFFFFF, 1.3);
     scene.add(ambientLight);
 
     // game logic
