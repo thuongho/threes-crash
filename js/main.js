6927698a586d7cc43cbc0b7d7d6c3882df9557b9
Thuong Ho <thuong.t.ho@gmail.com>
2016-05-02 00:35:42 -0700

    add stats

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 (function() {
   'use strict';
 
+    (function(){var script=document.createElement('script');script.onload=function(){var stats=new Stats();document.body.appendChild(stats.dom);requestAnimationFrame(function loop(){stats.update();requestAnimationFrame(loop)});};script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';document.head.appendChild(script);})();
+
     var scene = new THREE.Scene(); 
     // objects look bigger closer to the camera
     // ratio, near clipping plane, far clipping plane
